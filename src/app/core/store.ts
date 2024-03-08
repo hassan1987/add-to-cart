16efac22de93ac9db6e197ee0b8839d9c4455ff9
elhassan <elhassan.elgunaoui@gmail.com>
2024-03-08 01:18:51 +0100

    create generic store with RxJS

--- src/app/core/store.ts
+++ src/app/core/store.ts
@@ -1,2 +1,27 @@
-export class Store {
+import { BehaviorSubject, Observable } from "rxjs";
+
+export class Store<T> {
+    
+    public state$: Observable<T>;
+    private _state$: BehaviorSubject<T>;
+
+    private constructor(initialState: T) {
+       this._state$ = new BehaviorSubject<T>(initialState);
+       this.state$ = this._state$.asObservable();
+    }
+
+    //sync
+    get state(){
+        return this._state$.getValue();
+    }
+
+    protected setState(nextState: T){
+        console.log('--------------');
+        console.log('Previous State', this.state);
+
+        this._state$.next(nextState);
+
+        console.log('--------------');
+        console.log('Current State', this.state);
+    }
 }
